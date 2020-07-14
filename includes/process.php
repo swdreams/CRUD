065a43b74f3a50b8788f44457003c570698b4638
swdreams <likigoldenstar@gmail.com>
2020-07-14 23:33:49 +0300

    Updated delete logic. Code restructuring.

--- includes/process.php
+++ includes/process.php
@@ -1,37 +1,32 @@
-<?php 
+<?php
+
+session_start();
+//connect to database
+$conn = new mysqli($servername, $username, $passwd, $dbname);
+
+$action = isset($_GET['action']) ? $_GET['action'] : '';
+
+//delete button
+if ($action == 'delete') {
+    $uId = ($_GET['btnDelete']);
+    $sql = "DELETE FROM tb_user WHERE id = $uId";
+    $conn->query($sql);
+    $_SESSION['message'] = "Record has been deleted!";
+    $_SESSION['msg_type'] = "danger";
+    header("Location: index.php");
+}
+
+//submit button
+if (isset($_POST['btnSubmit'])) {
+    $fname = $_POST['input_Fname'];
+    $lname = $_POST['input_Lname'];
+    $gender = $_POST['input_Gender'];
+    $address = $_POST['input_Address'];
+    $sql = "INSERT INTO tb_user (first_name, last_name, gender, place) VALUES ('$fname','$lname','$gender','$address')";
+    $conn->query($sql);
+    $_SESSION['message'] = "Record has been saved!";
+    $_SESSION['msg_type'] = "success";
+    header("Location: index.php");
+}
 
 
-   $servername = '127.0.0.1';
-   $username = 'root';
-   $passwd = '';
-   $dbname = 'basiccrud';
-
-   session_start();
-   //connect to database
-   $conn = new mysqli($servername, $username, $passwd, $dbname);
-
-   //delete button
-   if(isset($_GET['btnDelete'])){
-      $uId = ($_GET['btnDelete']);
-      $sql = "DELETE FROM tb_user WHERE id = $uId";
-      $conn->query($sql);
-      $_SESSION['message'] = "Record has been deleted!";
-      $_SESSION['msg_type'] = "danger";
-      header("Location: index.php");
-   }
-
-   //submit button
-   if(isset($_POST['btnSubmit'])){
-      $fname = $_POST['input_Fname'];
-      $lname = $_POST['input_Lname'];
-      $gender = $_POST['input_Gender'];
-      $address = $_POST['input_Address'];
-      $sql = "INSERT INTO tb_user (first_name, last_name, gender, place) VALUES ('$fname','$lname','$gender','$address')";
-      $conn->query($sql);
-      $_SESSION['message'] = "Record has been saved!";
-      $_SESSION['msg_type'] = "success";
-      header("Location: index.php");
-   }
-
-
-?>
--- index.php
+++ index.php
@@ -1,96 +1,98 @@
+<?php
+include_once 'includes/dbconfig.php';
 
+include_once 'includes/process.php';
+
+?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>PHP CRUD</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <style> 
-        body{
-          width: 80%;
-          margin: auto;
+    <style>
+        body {
+            width: 80%;
+            margin: auto;
 
         }
     </style>
-  </head>
-  <body>
-    <!-- include php files -->
-    <?php include_once 'includes/process.php' ?>
+</head>
+<body>
+<?php
 
-    <?php 
-      
-      if (isset($_SESSION['message'])):
+if (isset($_SESSION['message'])):
 
     ?>
 
-    <div class="alert alert-<?=$_SESSION['msg_type'] ?>">
-        <?php 
-          echo $_SESSION['message'];
-          unset($_SESSION['message']);
+    <div class="alert alert-<?= $_SESSION['msg_type'] ?>">
+        <?php
+        echo $_SESSION['message'];
+        unset($_SESSION['message']);
         ?>
-  </div>
-        <?php endif ?>
-    <h1>Basic CRUD</h1>
-    <form action="" method="post" id="form" autocomplete="off" >
+    </div>
+<?php endif ?>
+<h1>Basic CRUD</h1>
+<form action="" method="post" id="form" autocomplete="off">
 
-        <div class="form-group"> 
-          <label for="">First name: </label>
-          <input type="text" class="col-xs-3" name='input_Fname'>
-        </div>
+    <div class="form-group">
+        <label for="">First name: </label>
+        <input type="text" class="col-xs-3" name='input_Fname'>
+    </div>
 
-        <div class="form-group"> 
-          <label for="">Last name: </label>
-          <input type="text" class="col-xs-3" name='input_Lname'>
-        </div>
+    <div class="form-group">
+        <label for="">Last name: </label>
+        <input type="text" class="col-xs-3" name='input_Lname'>
+    </div>
 
-        <div class="form-group"> 
-          <label for="">Gender: </label>
-            <input type="radio" name='input_Gender' style='margin-left: 20px;' value='Male' checked="checked"> Male
-            <input type="radio" name='input_Gender' style='margin-left: 20px;' value='Female' > Female
-        </div>
+    <div class="form-group">
+        <label for="">Gender: </label>
+        <input type="radio" name='input_Gender' style='margin-left: 20px;' value='Male' checked="checked"> Male
+        <input type="radio" name='input_Gender' style='margin-left: 20px;' value='Female'> Female
+    </div>
 
-        <div class="form-group"> 
-          <label for="">Address: </label>
-          <input type="text" class="col-xs-3" style='margin-left: 18px;' name='input_Address'>
-        </div>
+    <div class="form-group">
+        <label for="">Address: </label>
+        <input type="text" class="col-xs-3" style='margin-left: 18px;' name='input_Address'>
+    </div>
 
-        <div class="form-group col-xs-1">
-          <input type="submit" name='btnSubmit' class="btn btn-primary btn-md" value="Submit">
-          <input type="button" class="btn btn-danger" value="Clear" onClick="btnClear()"; >
-        </div>
+    <div class="form-group col-xs-1">
+        <input type="submit" name='btnSubmit' class="btn btn-primary btn-md" value="Submit">
+        <input type="button" class="btn btn-danger" value="Clear" onClick="btnClear()" ;>
+    </div>
 
-        <div class="form-group">
-          <table class='table'>
-              <thead>
+    <div class="form-group">
+        <table class='table'>
+            <thead>
+            <tr>
+                <th>First name</th>
+                <th>Last name</th>
+                <th>Gender</th>
+                <th>Location</th>
+                <th colspan="2">Action</th>
+            </tr>
+            </thead>
+            <?php
+            $result = $conn->query('SELECT * FROM tb_user ORDER BY id DESC') or die($conn->error);
+            while ($row = $result->fetch_object()):?>
                 <tr>
-                  <th>First name</th>
-                  <th>Last name</th>
-                  <th>Gender</th>
-                  <th>Location</th>
-                  <th colspan="2">Action</th>
+                    <td><?php echo $row->first_name; ?> </td>
+                    <td><?php echo $row->last_name; ?></td>
+                    <td><?php echo $row->gender; ?></td>
+                    <td><?php echo $row->place; ?></td>
+                    <td colspan="2">
+                        <a href="index.php?action=edit&id=<?php echo $row->id; ?>" class="btn btn-info">Edit</a>
+                        <a href="index.php?action=delete&id==<?php echo $row->id; ?>" class="btn btn-danger">Delete</a>
+                    </td>
                 </tr>
-              </thead>
-              <?php
-                $result = $conn->query('SELECT * FROM tb_user ORDER BY id DESC') or die($conn->error);
-                while($row = $result->fetch_object()):?>
-                <tr>
-                  <td><?php echo $row->first_name; ?> </td>
-                  <td><?php echo $row->last_name; ?></td>
-                  <td><?php echo $row->gender; ?></td>
-                  <td><?php echo $row->place; ?></td>
-                  <td colspan="2">                 
-                    <a href="index.php?edit<?php echo $row->id; ?>" class="btn btn-info">Edit</a>
-                    <a href="includes/process.php?delete=<?php echo $row->id; ?>" class="btn btn-danger">Delete</a>
-                  </td>
-                </tr>
-                    <?php endwhile; ?>
-            </table>
-          </div>
-    </form>
-    <script src="script/script.js"></script>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-  </body>
+            <?php endwhile; ?>
+        </table>
+    </div>
+</form>
+<script src="script/script.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
 </html>
